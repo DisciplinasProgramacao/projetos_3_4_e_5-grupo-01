6c13b71c86fc3d377d54e460d51d4d4d492f1f32
diegooow <62667876+diegooow@users.noreply.github.com>
2023-04-18 09:50:36 -0300

    Update PlataformaStreaming.java

--- codigo/PlataformaStreaming.java
+++ codigo/PlataformaStreaming.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class PlataformaStreaming{
 	private String nome;
@@ -28,6 +30,34 @@ public class PlataformaStreaming{
 	}
 	public void setClienteAtual(Cliente clienteAtual) {
 		ClienteAtual = clienteAtual;
+	}
+	
+	/*
+	 * Metódo para adicionar o cliente na tabela
+	 * 
+	 * @param cliente indica o cliente a ser adicionado
+	 */
+	public void adicionarCliente(Cliente cliente) {
+		this.clientes.put(nome, cliente);
+	}
+
+	/*
+	 * filtra o genero passado por parametro e adiciona na lista filtrada
+	 * 
+	 * @param genero indica o genero a ser buscado
+	 * 
+	 * @return Uma lista do tipo Serie com os generos buscados
+	 */
+	public Lista<Serie> filtrarPorGenero(String genero) {
+		Lista<Serie> listaFiltrada = new Lista<Serie>();
+		for (Entry<String, Serie> entry : series.entrySet()) {
+			Serie s = entry.getValue();
+			if (s.getGenero().equals(genero)) {
+				listaFiltrada.add(s);
+			}
+		}
+
+		return listaFiltrada;
 	}
 
 }
