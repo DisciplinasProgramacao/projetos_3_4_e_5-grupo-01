a28e5aff64a3e5b3fcfa18472065ae50e02348b6
Klaus8906 <107149823+Klaus8906@users.noreply.github.com>
2023-04-25 09:50:58 -0300

    Create Filme.java

---
+++ codigo/Filme.java
@@ -0,0 +1,56 @@
+public class Filme {
+	private String nome;
+	private String genero;
+	private String idioma;
+	private int duracaoSeg;
+	private int audiencia;
+  
+	public Filme(String nome, String genero, String idioma, int duracaoSeg, int audiencia) {
+		this.nome = nome;
+		this.genero = genero;
+		this.idioma = idioma;
+		this.duracaoSeg = duracaoSeg;
+		this.audiencia = audiencia;
+	}
+  
+	public String getNome() {
+		return nome;
+	}
+  
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+  
+	public String getGenero() {
+		return genero;
+	}
+  
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+  
+	public String getIdioma() {
+		return idioma;
+	}
+  
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+  
+	public int duracaoSeg() {
+		return duracaoSeg;
+	}
+  
+	public void setQuantidadeEpisodios(int duracaoSeg) {
+		this.duracaoSeg = duracaoSeg;
+	}
+  
+	public int getAudiencia() {
+		return audiencia;
+	}
+  
+	public void setAudiencia(int audiencia) {
+		this.audiencia = audiencia;
+	}
+	
+}
