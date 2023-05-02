de3d68a7f4babb9ceed06c987bb2c0f6fa28344d
Klaus8906 <107149823+Klaus8906@users.noreply.github.com>
2023-05-02 09:55:50 -0300

    Update Serie.java

--- codigo/Serie.java
+++ codigo/Serie.java
@@ -1,23 +1,26 @@
-import java.util.List;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
+package proj345;
+
+import java.util.Random;
 
 public class Serie {
 	private static final String[] GENEROS = {"romance", "acao", "comedia"};
+	private static final String[] IDIOMAS = {"PT-BR", "PT-PG", "ENG"};
 	private String nome;
 	private String genero;
 	private String idioma;
 	private int quantidadeEpisodios;
 	private int audiencia;
+	private long idSerie;
+	private String dataLancamento;
   
-	public Serie(String nome, String genero, String idioma, int quantidadeEpisodios, int audiencia) {
+	public Serie(String nome, String genero, String idioma, int quantidadeEpisodios, int audiencia, long idSerie, String dataLancamento) {
 		this.nome = nome;
 		this.genero = genero;
 		this.idioma = idioma;
 		this.quantidadeEpisodios = quantidadeEpisodios;
 		this.audiencia = audiencia;
+		this.idSerie = idSerie;
+		this.dataLancamento = dataLancamento;
 	}
   
 	public String getNome() {
@@ -63,6 +66,21 @@ public class Serie {
 	public static String[] getGeneros() {
 		return GENEROS;
 	}
+	public long getIdSerie() {
+		return idSerie;
+	}
+
+	public void setIdSerie(long idSerie) {
+		this.idSerie = idSerie;
+	}
+
+	public String getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(String dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
 
   //incrementa a audiencia da serie toda vez que ela for assistida por algum Cliente.
 	public void registrarAudiencia(){
@@ -70,19 +88,17 @@ public class Serie {
     setAudiencia(this.audiencia + 1);
   
   }
-  public void carregaSerie(String caminhoArquivo, Serie serie) throws IOException {
-        String linha;
+  public Serie carregaSerie(String linha) {
         String regex = ",";
         String[] campos = null;
+        Random gerador = new Random();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
-            linha = br.readLine();
             if (linha != null) {
                 campos = linha.split(regex);
+                Serie serie = new Serie (campos[1], GENEROS[gerador.nextInt(3)], IDIOMAS[gerador.nextInt(3)], gerador.nextInt(24), gerador.nextInt(0, 100000), Integer.parseInt(campos[0]), campos[2]);
+                return serie;
             }
-        } catch (IOException e) {
-            System.out.println("Ocorreu um erro no arquivo");
-        }
-
+            return null;
     }
+
 }
