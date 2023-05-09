c1602cf371ad1f28a64b3bb792d0ae25c7a0b01d
Tabosaaa <67617287+Tabosaaa@users.noreply.github.com>
2023-05-09 10:46:15 -0300

    Add files via upload

---
+++ codigo/Avaliacao.java
@@ -0,0 +1,27 @@
+
+public class Avaliacao {
+
+	private float nota;
+	private String comentario;
+	private String data;
+	
+	public float getNota() {
+		return nota;
+	}
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+}
