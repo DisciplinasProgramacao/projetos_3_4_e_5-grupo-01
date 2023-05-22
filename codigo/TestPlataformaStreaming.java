391ab21be322470a669c1fa02a533a2b60524c74
Arthur-Teles <107149823+Klaus8906@users.noreply.github.com>
2023-05-22 20:03:59 -0300

    testes

--- codigo/TestPlataformaStreaming.java
+++ codigo/TestPlataformaStreaming.java
@@ -2,9 +2,9 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.HashMap;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class TestPlataformaStreaming {
     private PlataformaStreaming plataforma;
--- codigo/TestSerie.java
+++ codigo/TestSerie.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+
 public class TestSerie{
 
   Serie s1;
