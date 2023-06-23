2f67730648496e9cf4a3443fd9e3d4296f82f5da
diegooow <62667876+diegooow@users.noreply.github.com>
2023-06-23 20:59:32 -0300

    correcao do teste serie

--- codigo/test/SerieTest.java
+++ codigo/test/SerieTest.java
@@ -7,21 +7,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 
-public class SerieTest{
+public class SerieTest {
 
-  Serie s1;
-  
-  @Before
-	void setUp() throws Exception {
-   s1 = new Serie("Serie teste1", "Drama" , "Português" , 5 , 0, 1, "01/1/2000");
-  }
+	Serie s1;
 
-  @Test
-  public void registrarAudienciaTest(){
+	@Before
+	public void setUp() throws Exception {
+		s1 = new Serie("Serie teste1", Generos.ACAO, Idiomas.ENG, 5, 0, 1, "01/1/2000");
+	}
 
-    s1.registrarAudiencia();
+	@Test
+	public void registrarAudienciaTest() {
 
-    assertEquals(1, s1.getAudiencia());
-    
-  }
+		s1.registrarAudiencia();
+
+		assertEquals(1, s1.getAudiencia());
+	}
 }
