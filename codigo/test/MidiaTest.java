5965a30ec06f39ac3cdbf4fb31a7b83a07605ab0
diegooow <62667876+diegooow@users.noreply.github.com>
2023-06-24 12:05:24 -0300

    midia test

--- codigo/test/MidiaTest.java
+++ codigo/test/MidiaTest.java
@@ -4,22 +4,36 @@ import org.junit.Test;
 import Avaliacao;
 import Cliente;
 import Midia;
+import excecoes.usuarioNaoPodeComentarException;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 
 public class MidiaTest {
-
-    private Midia midia;
+	private Midia midia;
+    private Midia midia1;
+    private Midia midia2;
+    private Midia midia3;
+    private Midia midia4;
+    private Midia midia5;
+    private Midia midia6;
     private Cliente c1;
     private Cliente c2;
+    private Cliente c3;
 
     @Before
     public void setUp() {
-        midia = new Midia("Nome da Mídia", "Gênero", "Idioma", 0, "Data de Lançamento", 1);
-        c1 = new Cliente("Cliente teste1", "1234", "login");
-        c2 = new Cliente("Cliente teste2", "4321", "login2");
+    	midia = new Midia("nome", Generos.ACAO, Idiomas.ENG, 3, "23/06/2023", 4);
+        midia1 = new Midia("nome", Generos.ACAO, Idiomas.ENG, 3, "23/06/2023", 4);
+        midia2 = new Midia("nome", Generos.ACAO, Idiomas.ENG, 3, "23/06/2023", 4);
+        midia3 = new Midia("nome", Generos.ACAO, Idiomas.ENG, 3, "23/06/2023", 4);
+        midia4 = new Midia("nome", Generos.ACAO, Idiomas.ENG, 3, "23/06/2023", 4);
+        midia5 = new Midia("nome", Generos.ACAO, Idiomas.ENG, 3, "23/06/2023", 4);
+        midia6 = new Midia("nome", Generos.ACAO, Idiomas.ENG, 3, "23/06/2023", 4);
+        c1 = new clienteComum("Cliente teste1", "1234", "login");
+        c2 = new clienteEspecialista("Cliente teste2", "4321", "login2");
+        c3 = new clienteProfissional("Cliente teste3", "4321", "login3");
     }
     @Test
     public void testRegistrarAudiencia() {
@@ -30,32 +44,76 @@ public class MidiaTest {
     }
     @Test
     public void testAvaliar() {
-        Avaliacao avaliacao1 = new Avaliacao();
-        Avaliacao avaliacao2 = new Avaliacao();
+        Avaliacao avaliacao1 = new Avaliacao(5, "12/02/2006");
+        Avaliacao avaliacao2 = new Avaliacao(3, "dsadsada","12/02/2006");
+        c2.adicionaNaListaVistas(midia1);
+        c2.adicionaNaListaVistas(midia2);
+        c2.adicionaNaListaVistas(midia3);
+        c2.adicionaNaListaVistas(midia4);
+        c2.adicionaNaListaVistas(midia5);
 
-        boolean resultado1 = midia.avaliar(c1, avaliacao1);
-        boolean resultado2 = midia.avaliar(c2, avaliacao2);
+         try {
+			midia.adicionarAvaliacao(c1, avaliacao1);
+			midia.adicionarAvaliacao(c2, avaliacao2);
+		} catch (usuarioNaoPodeComentarException e) {
+			
+		}
 
-        assertTrue(resultado1);
-        assertTrue(resultado2);
-
+         assertFalse(c2.isComum());
+        
         assertEquals(avaliacao1, midia.getNotas().get(c1));
         assertEquals(avaliacao2, midia.getNotas().get(c2));
     }
     @Test
     public void testCalcMedia() {
-        Avaliacao avaliacao1 = new Avaliacao();
-        avaliacao1.setNota(5);
-        Avaliacao avaliacao2 = new Avaliacao();
-        avaliacao2.setNota(4);
-        Avaliacao avaliacao3 = new Avaliacao();
-        avaliacao3.setNota(3);
+    	Avaliacao avaliacao1 = new Avaliacao(5, "12/02/2006");
+        Avaliacao avaliacao2 = new Avaliacao(10, "12/02/2006");
+        Avaliacao avaliacao3 = new Avaliacao(3, "12/02/2006");
 
-        midia.avaliar(c1, avaliacao1);
-        midia.avaliar(c2, avaliacao2);
-        midia.avaliar(c1, avaliacao3);
+        
+        try {
+			midia.adicionarAvaliacao(c1, avaliacao3);
+			midia.adicionarAvaliacao(c3, avaliacao1);
+	        midia.adicionarAvaliacao(c2, avaliacao2);
+		} catch (usuarioNaoPodeComentarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         double media = midia.calcMedia();
-        assertEquals(4.0, media, 0.001);
+        assertEquals(6.0, media, 0.001);
+    }
+    
+    @Test
+    public void testJaAssistiu() {
+    	c1.adicionaNaListaVistas(midia1);
+    	assertFalse(c1.jaAssistiu(midia));
+    	assertTrue(c1.jaAssistiu(midia1));
+    }
+    
+    @Test
+    public void testTipoCliente(){
+    	Avaliacao avaliacao1 = new Avaliacao(5, "12/02/2006");
+        Avaliacao avaliacao2 = new Avaliacao(3, "dsadsada","12/02/2006");
+        c1.adicionaNaListaVistas(midia1);
+        c1.adicionaNaListaVistas(midia2);
+        c1.adicionaNaListaVistas(midia3);
+        c1.adicionaNaListaVistas(midia4);
+        
+        c2.adicionaNaListaVistas(midia1);
+        c2.adicionaNaListaVistas(midia2);
+        c2.adicionaNaListaVistas(midia3);
+        c2.adicionaNaListaVistas(midia4);
+        c2.adicionaNaListaVistas(midia5);
+        
+        try {
+			assertTrue(c2.isEspecialista());
+			assertFalse(c1.isEspecialista());
+		} catch (usuarioNaoPodeComentarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        assertFalse(c2.isComum());
+        assertTrue(c1.isComum());
     }
 }
